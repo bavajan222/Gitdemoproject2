b727f1c01924cd1cb9109ad3b009965544635c4f
Shaik <Shaik@DESKTOP-23UJABP>
2020-02-06 15:43:09 +0530

    First commit

---
+++ src/demoproject/Test1.java
@@ -0,0 +1,17 @@
+package demoproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+System.out.println("test one pass");
+WebDriver driver=new ChromeDriver();
+driver.get("http:\\google.com");
+		
+		
+	}
+
+}
